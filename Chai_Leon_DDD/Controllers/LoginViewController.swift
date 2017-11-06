457c121ab47b7256942d2eb2550543919f573be9
Leon Chai <Leonchai964@gmail.com>
2017-11-06 14:13:19 -0800

    made login view controller but no function yet

---
+++ Chai_Leon_DDD/Controllers/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Chai_Leon_DDD
+//
+//  Created by Leon Chai on 2017-11-06.
+//  Copyright © 2017 Leon Chai. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+	@IBOutlet weak var emailTxtField: UITextField!
+	@IBOutlet weak var passwordTxtField: UITextField!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
